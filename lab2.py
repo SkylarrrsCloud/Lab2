b58b14c91c7d3163dc5093e97ea1fcd3acfce274
Edwin <edwinlim2003@gmail.com>
2025-10-28 15:30:32 +0800

    main and user input

--- lab2.py
+++ lab2.py
@@ -1,1 +1,16 @@
 print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+def display_main_menu():
+    print("Enter some numbers separated by commas (e.g. 5, 67, 32)")
+def get_user_input():
+    user_input = input("Enter Number: ")
+    string_list = user_input.split(",")
+    float_list = [float(num) for num in string_list]
+    return float_list
+def calc_average():
+    print("calc_average")
+def find_min_max():
+    print("find_min_max")
+def sort_temperature():
+    print("sort_temperature")
+def calc_median_temperature():
+    print("calc_median_temperature")
